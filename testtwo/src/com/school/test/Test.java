b420916d9b4371fad48cfe9937a0a41e0992f6d2
zq1146787512 <1146787512@qq.com>
2017-03-01 15:40:47 +0800

    Signed-off-by: zq1146787512 <1146787512@qq.com>

--- testtwo/src/com/school/test/Test.java
+++ testtwo/src/com/school/test/Test.java
@@ -5,17 +5,7 @@ import java.io.IOException;
 import com.school.utils.ImageHelper;
 
 public class Test {
-	// public void gethour() {
-	// String name = "12-SS";
-	//
-	// System.out.println(name.substring(3, 5));
-	// }
-	//
-	// public static void main(String[] args) {
-	// for (int i = 10; i < 5; i++) {
-	// System.out.println("ss");
-	// }
-	// }
+	
 	public static void main(String[] args) {
 		try {
 			ImageHelper.cutImage("image/error1.jpg", 1,"f:/image/iamge");
